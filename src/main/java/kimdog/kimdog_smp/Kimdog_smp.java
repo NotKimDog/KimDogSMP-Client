cceddfeaf2b78993f5ae552693127dcfefd45731
NotKimDog <kim.dog.543@gmail.com>
2026-01-17 19:40:12 +0000

    Bump version to 1.0.0-patch

--- src/main/java/kimdog/kimdog_smp/Kimdog_smp.java
+++ src/main/java/kimdog/kimdog_smp/Kimdog_smp.java
@@ -14,7 +14,7 @@ import org.slf4j.LoggerFactory;
 
 public class Kimdog_smp implements ModInitializer {
     private static final Logger LOGGER = LoggerFactory.getLogger("KimDog SMP");
-    private static final String VERSION = "1.0.0-major";
+    private static final String VERSION = "1.0.0-patch";
     private static final String MC_VERSION = "1.0";
 
     private static int modulesLoaded = 0;
